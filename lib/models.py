b90dce2963a510b8a209bfc29dd674361a4b15f7
Rmrti <mutuma34@gmail.com>
2025-03-11 13:23:44 +0300

    CREATE: the Auditions table

--- lib/models.py
+++ lib/models.py
@@ -9,17 +9,31 @@ metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
 
+tb_id = Column(Integer, primary_Key= True, autoincrement=True)
+
 class Actor(Base):
     __tablename__ = "actors"
-    id = Column(Integer, primary_Key= True, autoincrement=True)
+    id = tb_id
 
     name = Column(String, nullable=False)
     auditions = relationship("Audition", back_populates="actor")
 class Role(Base):
     __tablename__ = "roles"
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = tb_id
     character_name=Column(String, nullable=False)
     auditions = relationship("Audition", back_populates="actor")
 
 
+class Audition(Base):
+    __tablename__ = "auditions"
+    id = tb_id
+    actor_id = Column(Integer, ForeignKey("actors.id"), nullable=False)
+    role_id = Column(Integer, ForeignKey("roles.id"), nullable=False)
+    location= Column(String, nullable=False)
+    phone = Column(Integer, nullable=False)
+    hired= Column(Boolean, nullable=False)
 
+    #Relationships
+    actor = relationship("Actor", back_populates="auditions")
+    role = relationship("Role", back_populates="auditions")
+    
